daa862b4b45b5c836840e7a9be7683886f2ce059
edward <clem0609@mail.ru>
2020-04-28 16:30:29 +1000

    Добавил отображение календаря или времени для полей ввода

--- app/src/main/java/ru/nekrasoved/diagnosticcar/AddLog.java
+++ app/src/main/java/ru/nekrasoved/diagnosticcar/AddLog.java
@@ -2,13 +2,73 @@ package ru.nekrasoved.diagnosticcar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AddLog extends AppCompatActivity {
+
+    EditText etDate;
+    EditText etTime;
+    Calendar dateAndTime=Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_log);
+
+        etDate = (EditText) findViewById(R.id.etDate);
+        etTime = (EditText) findViewById(R.id.etTime);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate();
+            }
+        });
+
+        etTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTime();
+            }
+        });
+
     }
+
+    public void setDate() {
+        new DatePickerDialog(AddLog.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    public void setTime() {
+        new TimePickerDialog(AddLog.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
+    }
+
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            etTime.setText(hourOfDay + ":" + minute);
+        }
+    };
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            etDate.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+        }
+    };
+
 }
