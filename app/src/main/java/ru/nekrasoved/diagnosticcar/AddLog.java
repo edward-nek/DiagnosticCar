3a2b6971a214cfc64b15a89e7b66816799e02e3a
edward <clem0609@mail.ru>
2020-04-28 16:07:39 +1000

    Add design: 1) Add log activity 2) List data

---
+++ app/src/main/java/ru/nekrasoved/diagnosticcar/AddLog.java
@@ -0,0 +1,14 @@
+package ru.nekrasoved.diagnosticcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_log);
+    }
+}
--- app/src/main/java/ru/nekrasoved/diagnosticcar/MainActivity.java
+++ app/src/main/java/ru/nekrasoved/diagnosticcar/MainActivity.java
@@ -3,12 +3,20 @@ package ru.nekrasoved.diagnosticcar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    DataDB dataDB;
+    Cursor cursor;
+
+    private ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,31 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Список данных: ");
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#DB001B")));
+
+        showList();
+    }
+
+    public void showList(){
+        dataDB = new DataDB(this);
+        cursor = dataDB.getAllData();
+
+        list = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+
+            int idIndex = cursor.getColumnIndex(DataDB.KEY_ID);
+            int dateIndex = cursor.getColumnIndex(DataDB.KEY_DATE);
+
+
+            do {
+                list.add(idIndex,  cursor.getString(idIndex) +
+                        ") " + cursor.getString(dateIndex) + " ");
+
+            } while (cursor.moveToNext());
+        }
+
+
+
     }
 
 }
